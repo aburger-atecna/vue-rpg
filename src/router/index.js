ea2f59166e01edd5c6abfa53ac05a4ec0a6f7c18
aburger <aburger@atecna.fr>
2022-03-21 10:46:52 +0100

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Dashboard from '../components/auth/dashboard';
 import Home from '../components/home';
+import Login from '../components/auth/login';
+import Register from '../components/auth/register';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   },
   {
     path: '/',
